606c76164bb5322d67d54d48378d3968be4f9aa9
Bedirhan <bedirhansiyam@gmail.com>
2023-03-11 11:54:37 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace IfElse_TernaryIf;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        // if - else if - else
+
+        if(time >= 6 && time < 11)
+            Console.WriteLine("Good morning!");
+        else if(time <= 18)
+            Console.WriteLine("Have a nice day.");
+        else
+            Console.WriteLine("Good night!");
+
+
+        // Ternary if
+
+        string result = time >= 6 && time < 11 ? "Good morning!" : time <= 18 ? "Have a nice day." : "Good night!";
+        Console.WriteLine(result);
+    }
+}
